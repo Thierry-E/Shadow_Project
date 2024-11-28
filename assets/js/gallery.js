5e65fbb67b522e0cb62d66a4ab498362914914f5
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-11-28 20:25:59 +0100

    Ajustement dynamique des chemins des images de la gallerie des certificats.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -6,16 +6,17 @@ const gallery = document.querySelector('.gallery')
 
 // Fonction pour ajuster les chemins des images en fonction de l'environnement
 function adjustPathsForGitHubPages(certificats) {
-  // Détection si nous sommes sur GitHub Pages (en fonction du hostname)
+  // Détection si nous sommes sur GitHub Pages
   const isGitHubPages = window.location.hostname === 'Thierry-E.github.io'
 
   if (isGitHubPages) {
     // Si nous sommes sur GitHub Pages, ajouter /Shadow_Project/ avant les chemins
-    const basePath = '/Shadow_Project' // Remplacez Shadow_Project par le nom de votre dépôt GitHub
+    const basePath = '/Shadow_Project'
 
+    // Parcourir chaque certificat et ajouter le préfixe de basePath
     certificats.forEach((certificat) => {
-      certificat.src = basePath + certificat.src // Chemin complet de l'image
-      certificat.thumbnail = basePath + certificat.thumbnail // Chemin complet de la miniature
+      certificat.src = basePath + certificat.src // Chemin complet pour l'image principale
+      certificat.thumbnail = basePath + certificat.thumbnail // Chemin complet pour la miniature
     })
   }
 }
